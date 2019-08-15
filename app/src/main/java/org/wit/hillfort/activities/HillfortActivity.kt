82357133847b5528e7fd718b1206186fe61754dd
JDonoghue <donoghuej97@gmail.com>
2019-08-15 17:10:49 +0100

    Added the HillfortModel Class, made changes to HillfortActivity.

---
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -0,0 +1,30 @@
+package org.wit.hillfort.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.models.HillfortModel
+
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+  var hillfort = HillfortModel()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_hillfort)
+
+    btnAdd.setOnClickListener() {
+      hillfort.title = hillfortTitle.text.toString()
+      if (hillfort.title.isNotEmpty()) {
+        info("add Button Pressed: $hillfort")
+      }
+      else {
+        toast("Please Enter a title")
+      }
+    }
+  }
+}

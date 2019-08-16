07bf7c7976536e48abd6a0432853f98d34e36f8f
JDonoghue <donoghuej97@gmail.com>
2019-08-16 20:14:04 +0100

    improved modelling, added the ability to edit cards or cancel the editing process

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -2,6 +2,8 @@ package org.wit.hillfort.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -19,16 +21,23 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
     app = application as MainApp
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
 
+    if (intent.hasExtra("hillfort_edit")) {
+      hillfort = intent.extras.getParcelable<HillfortModel>("hillfort_edit")
+      hillfortTitle.setText(hillfort.title)
+      description.setText(hillfort.description)
+    }
     btnAdd.setOnClickListener() {
       hillfort.title = hillfortTitle.text.toString()
       hillfort.description = description.text.toString()
 
 
       if (hillfort.title.isNotEmpty()) {
-        app.hillforts.add(hillfort.copy())
+        app.hillforts.create(hillfort.copy())
         info("Add Button Pressed: $hillfortTitle")
-        app.hillforts.forEach{info ("Add Button Pressed:  ${it}")}
+       // app.hillforts.findAll().forEach{info ("Add Button Pressed:  ${it}")}
         setResult(AppCompatActivity.RESULT_OK)
         finish()
       }
@@ -36,5 +45,17 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
         toast("Please Enter a title")
       }
     }
+  }
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_hillfort, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> {
+        finish()
+      }
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
